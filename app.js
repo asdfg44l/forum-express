6560e1c08b5c096639a01c73efd9368d5af2853e
Vincent <asdfg10439l@gmail.com>
2021-03-30 15:49:03 +0800

    add: add rest and admin controller

--- app.js
+++ app.js
@@ -1,7 +1,13 @@
 const express = require('express')
 const app = express()
+const exphbs = require('express-handlebars')
 const port = 3000
 
+//view engine
+app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
+app.set('view engine', 'hbs')
+
+//listening
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
 })
--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,16 @@
-module.exports = (app) => {
+const restController = require('../controllers/restController')
+const adminController = require('../controllers/adminController')
 
+module.exports = app => {
+  //rest
   app.get('/', (req, res) => {
-    res.send('Hello World!')
+    return res.redirect('/restaurants')
   })
+  app.get('/restaurants', restController.getRestaurants)
+
+  //admin
+  app.get('/admin', (req, res) => {
+    return res.redirect('/admin/restaurants')
+  })
+  app.get('/admin/restaurants', adminController.getRestaurants)
 }
