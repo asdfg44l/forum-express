170165d6ce1d20d9178e197fa9776bb4eb5b5afc
Vincent <asdfg10439l@gmail.com>
2021-03-30 17:11:14 +0800

    install body-parser

--- app.js
+++ app.js
@@ -1,12 +1,17 @@
 const express = require('express')
 const app = express()
 const exphbs = require('express-handlebars')
+const bodyParser = require('body-parser')
 const db = require('./models')
+const { urlencoded } = require('body-parser')
 const port = 3000
 
 //view engine
 app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
 app.set('view engine', 'hbs')
+
+//bodyParser
+app.use(bodyParser.urlencoded({ extended: true }))
 
 //listening
 app.listen(port, () => {
