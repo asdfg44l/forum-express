47581d1aec22b0d88fe52def054693ae2d08bac2
Vincent <asdfg10439l@gmail.com>
2021-04-01 18:49:43 +0800

    Install faker and add restaurant seed data

---
+++ seeders/20210401103641-restaurants-seed-file.js
@@ -0,0 +1,40 @@
+'use strict';
+const faker = require('faker')
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.bulkInsert('Restaurants',
+      Array.from({ length: 50 }).map((d, i) => {
+        return {
+          name: faker.name.findName(),
+          tel: faker.phone.phoneNumber(),
+          address: faker.address.streetAddress(),
+          opening_hours: '08:00',
+          image: `https://loremflickr.com/320/240/restaurant,food/?random=${Math.random() * 100}`,
+          description: faker.lorem.text(),
+          createdAt: new Date(),
+          updatedAt: new Date()
+        }
+      })
+    )
+    /**
+     * Add seed commands here.
+     *
+     * Example:
+     * await queryInterface.bulkInsert('People', [{
+     *   name: 'John Doe',
+     *   isBetaMember: false
+     * }], {});
+    */
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.bulkDelete('Restaurants', null, {})
+    /**
+     * Add commands to revert seed here.
+     *
+     * Example:
+     * await queryInterface.bulkDelete('People', null, {});
+     */
+  }
+};
