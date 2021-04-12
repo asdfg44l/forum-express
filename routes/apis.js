c4af74ad60ebcb8c7113087f9878f55a6b4d82c3
Vincent <asdfg10439l@gmail.com>
2021-04-12 16:02:37 +0800

    DELETE /api/admin/restaurants/:id

--- routes/apis.js
+++ routes/apis.js
@@ -6,6 +6,7 @@ const apiCategoryController = require('../controllers/api/categoryController')
 
 router.get('/admin/restaurants', apiAdminController.getRestaurants)
 router.get('/admin/restaurants/:id', apiAdminController.getRestaurant)
+router.delete('/admin/restaurants/:id', apiAdminController.deleteRestaurant)
 router.get('/admin/categories', apiCategoryController.getCategories)
 
 module.exports = router
